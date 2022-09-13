6122010c44422668ea64807f2188ad908a77a711
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-09-13 11:09:22 +0630

    Added Computed field for Age

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,4 +1,4 @@
-from email.policy import default
+from datetime import date
 from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
@@ -7,7 +7,17 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread','mail.activity.mixin']  #inherit for oe.chatter , mixin is for activity_ids
     
     name = fields.Char(string='Name', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    date_of_birth = fields.Date(string="Date of Birth")
+    age = fields.Integer(string='Age', compute="_compute_age", tracking=True)
     ref = fields.Char(string="Reference")
     gender = fields.Selection([('male', 'Male'),('female', 'Female')], string='Gender', tracking=True, default="male")
     active = fields.Boolean(string="Active", default="True")
+    
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for record in self:
+            today = date.today()
+            if record.date_of_birth:
+                record.age = today.year - record.date_of_birth.year
+            else:
+                record.age = 0
