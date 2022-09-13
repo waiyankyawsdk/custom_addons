c1bc95534b6c1fc3ef416082af6929f98b6a140d
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-09-13 13:54:47 +0630

    Added rec_name in appointment and Patients' Appointments

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -5,7 +5,7 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread','mail.activity.mixin']  
     _description = "Hospital Appointment"
-
+    _rec_name = "ref"
     patient_ids = fields.Many2one(string="Patient", comodel_name="hospital.patient", tracking=True)
     gender = fields.Selection(related="patient_ids.gender", readonly=False, tracking=True)
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now, tracking=True)
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -12,6 +12,7 @@ class HospitalPatient(models.Model):
     ref = fields.Char(string="Reference")
     gender = fields.Selection([('male', 'Male'),('female', 'Female')], string='Gender', tracking=True, default="male")
     active = fields.Boolean(string="Active", default="True")
+    appointment_ids = fields.Many2one('hospital.appointment', string="Appointments")    
     
     @api.depends('date_of_birth')
     def _compute_age(self):
