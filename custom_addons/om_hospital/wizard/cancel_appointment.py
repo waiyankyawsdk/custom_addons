8502e88a33f2e46a28873c7c416a61f44abf5467
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-09-28 13:14:54 +0630

    Added Wizard for Appointment Cancel

--- custom_addons/om_hospital/wizard/cancel_appointment.py
+++ custom_addons/om_hospital/wizard/cancel_appointment.py
@@ -6,3 +6,6 @@ class CancelAppointmentWizard(models.TransientModel):
     _description = "Cancel Appointment Wizard"
 
     appointment_id = fields.Many2one(comodel_name="hospital.appointment",string="Appointment")
+    reason = fields.Text()
+    def action_cancel(self):
+        return
