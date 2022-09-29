8a5cd162c75de1d296104245ef885b58de41dab6
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-09-29 10:33:26 +0630

    Added confirmed user after payment item inheritence

---
+++ custom_addons/om_odoo_inheritence/models/sale_order.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order" 
+    
+    confirmed_user_id = fields.Many2one("res.users", string="Confirmed User")
