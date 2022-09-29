ee15a2ece8d657593f6b1fdadec5bb655ef4a41e
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-09-29 14:05:33 +0630

    Added default_get in Cancel Wizard

--- custom_addons/om_hospital/wizard/cancel_appointment.py
+++ custom_addons/om_hospital/wizard/cancel_appointment.py
@@ -1,4 +1,4 @@
-from datetime import date
+import datetime
 from odoo import api, fields, models
 
 class CancelAppointmentWizard(models.TransientModel):
@@ -7,5 +7,13 @@ class CancelAppointmentWizard(models.TransientModel):
 
     appointment_id = fields.Many2one(comodel_name="hospital.appointment",string="Appointment")
     reason = fields.Text()
+    date_cancel = fields.Date(string="Cancellation Date")
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        res['date_cancel'] = datetime.date.today()
+        return res
+    
     def action_cancel(self):
         return
