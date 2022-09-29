ab1f4a2116f2ce7b6fc1b4aa9108741eb526db15
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2022-09-29 13:43:56 +0630

    Override Write method of Patient

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -21,6 +21,11 @@ class HospitalPatient(models.Model):
         vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
         return super().create(vals)
     
+    def write(self, vals):
+        if not self.ref and not vals.get('ref'):
+            vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super().write(vals)
+    
     @api.depends('date_of_birth')
     def _compute_age(self):
         for record in self:
