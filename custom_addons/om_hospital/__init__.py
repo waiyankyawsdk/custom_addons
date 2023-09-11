eaf67e48296d022588d59886f53eee9ded63853d
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2023-09-11 14:12:32 +0630

    Last Commit (11/09/23)

--- custom_addons/om_hospital/__init__.py
+++ custom_addons/om_hospital/__init__.py
@@ -1,2 +1,9 @@
 from . import models
 from . import wizard
+from  . import hooks
+
+def post_init_hook(cr, registry):
+    hooks.test_post_init_test(cr, registry)
+
+def post_load():
+    hooks.post_load_hook()
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,67 +1,92 @@
+# -*- coding: utf-8 -*-
 from datetime import date
+from dateutil import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
-from dateutil import relativedelta
+
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
-    _inherit = ['mail.thread','mail.activity.mixin']  #inherit for oe.chatter , mixin is for activity_ids
-    
+    _inherit = ['mail.thread','mail.activity.mixin']
+    #inherit for oe.chatter , mixin is for activity_ids
+
     name = fields.Char(string='Name', tracking=True)
     date_of_birth = fields.Date(string="Date of Birth")
-    age = fields.Integer(string='Age', compute="_compute_age", inverse="_inverse_compute_age", search="_search_age", tracking=True)
+    age = fields.Integer(string='Age', compute="_compute_age", inverse="_inverse_compute_age",
+                         search="_search_age", tracking=True)
     ref = fields.Char(string="Reference")
     gender = fields.Selection([('male', 'Male'),('female', 'Female')], string='Gender', tracking=True, default="male")
     active = fields.Boolean(string="Active", default="True")
-    appointment_id = fields.Many2one('hospital.appointment', string="Appointment")    
+    appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
     image = fields.Image(string="Image")
     tag_ids = fields.Many2many('patient.tag', string="Tags")
     appointment_count = fields.Integer(compute="_compute_appointment_count", store=True)
     appointment_ids = fields.One2many('hospital.appointment', 'patient_id', string="Appointments")
     parent = fields.Char()
-    martial_status = fields.Selection([('single','Single'),('married','Married')], string="Martial Status", tracking=True)
+    martial_status = fields.Selection([('single','Single'),('married','Married')], string="Martial Status",
+                                      tracking=True)
     partner_name = fields.Char()
     is_birthday = fields.Boolean(string="Birthday?", compute="_compute_is_birthday")
-    
+    phone = fields.Char()
+    email = fields.Char()
+    website = fields.Char()
+
     @api.constrains('date_of_birth')
     def _check_date_of_birth(self):
         for record in self:
             if record.date_of_birth and record.date_of_birth > fields.Date.today():
                 raise ValidationError(_('The entered date cannot be greater than Today!'))
-    
+
     @api.depends('age')
     def _inverse_compute_age(self):
         today = date.today()
         for rec in self:
             rec.date_of_birth = today - relativedelta.relativedelta(years=rec.age)
-    
+
     def _search_age(self, operator, value):
         date_of_birth = date.today() - relativedelta.relativedelta(years=value)
         start_of_year = date_of_birth.replace(day=1, month=1)
         end_of_year = date_of_birth.replace(day=31, month=12)
         return [('date_of_birth','>=', start_of_year),('date_of_birth','<=', end_of_year)]
-        
+
     @api.depends('appointment_ids')
     def _compute_appointment_count(self):
         for rec in self:
-            rec.appointment_count = self.env['hospital.appointment'].search_count([('patient_id','=',rec.id)])
-            
+            rec.appointment_count = self.env['hospital.appointment'].search_count(
+                [('patient_id','=',rec.id)])
+
     @api.ondelete(at_uninstall=False)
     def _check_appointment(self):
         for rec in self:
             if rec.appointment_ids:
                 raise ValidationError(_("You can't delete a patient with appointments!"))
-                
+
     @api.model
     def create(self, vals):
+        """_summary_
+
+        Args:
+            vals (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
         return super().create(vals)
-    
+
     def write(self, vals):
+        """_summary_
+
+        Args:
+            vals (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         if not self.ref and not vals.get('ref'):
             vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
         return super().write(vals)
-    
+
     @api.depends('date_of_birth')
     def _compute_age(self):
         for record in self:
@@ -70,10 +95,15 @@ class HospitalPatient(models.Model):
                 record.age = today.year - record.date_of_birth.year
             else:
                 record.age = 1
-    
+
     def name_get(self):
-        return [(record.id, "[%s] %s" % (record.ref, record.name)) for record in self]
-    
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return [(record.id, f"[%s] %s" % (record.ref, record.name)) for record in self]
+
     @api.depends('date_of_birth')
     def _compute_is_birthday(self):
         for rec in self:
