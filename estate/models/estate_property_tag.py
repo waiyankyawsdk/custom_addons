2cd2331ba4db2648f2daa15767ccb15b0e59c0cf
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2023-09-13 14:01:08 +0630

    advanced view(pivot, graph, kanban)

--- estate/models/estate_property_tag.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class EstatePropertyTag(models.Model):
-    _name = "estate.property.tag"
-    _description = "Property Tag"
-
-    name = fields.Char(required = True)
-    property_ids = fields.Many2many("estate.property")
--- estate/models/estate_property_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class EstatePropertyType(models.Model):
-    _name = "estate.property.type"
-    _description = "Property Types"
-
-    name = fields.Char(required = True)
