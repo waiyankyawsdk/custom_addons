ef27f77839f4376b88100c9147a01e2e579e18fd
Wai Yan Kyaw <waiyankyawsdk1999@gmail.com>
2023-09-15 11:45:41 +0630

    Add new Travel Manager Module

---
+++ custom_addons/travel_manager/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Travel Manager',
+    'version' : '1.1',
+    'category' : 'Extra Tools',
+    'depends' : ['base', 'mail', 'uom'],
+    'description' : """
+    """,
+    'data' :[
+        'security/ir.model.access.csv',
+        'views/travel_view.xml'
+    ],
+    'demo':[
+
+    ],
+    'installable':True,
+    'licence' : 'LGPL-3'
+}
